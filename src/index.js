c5cb6b7c96496ee5945bff321e9d1179dd89d4cd
Maksim Egorov <maxceas228@gmail.com>
2023-01-11 22:57:28 +0300

    made a task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+ module.exports = function reverse (n) {
+    let str = n.toString();
+    let result = str.split('').reverse().join('');
+    
+    if (str.startsWith('-')) {
+       result = str.slice(1).split('').reverse().join('');
+    } 
+return Number(result);
 }
+   console.log(module.exports(-055))
